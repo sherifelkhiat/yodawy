3d1cd2786d1f30850afcb86adcba4b84910e0bc9
sherif elkhiat <sherif.elkhiat@57357.org>
2022-01-22 16:09:52 +0200

    commit changed files

---
+++ storage/framework/views/e58c92902a562515a6f0096dd262a16a002a7a9e.php
@@ -0,0 +1,18 @@
+<?php $__env->startSection('content'); ?>
+<div class="container">
+    <div class="row justify-content-center">
+         
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">List of world news</div>
+                     
+                <div class="card-body __jsApiKey" data-apiKey="bc365e1195eb436a9fc733803737507b">
+                  <example-component></example-component>
+                </div>
+                 
+            </div>
+        </div>
+    </div>
+</div>
+<?php $__env->stopSection(); ?> 
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /var/www/html/yodawy-app/resources/views/welcome.blade.php ENDPATH**/ ?>
